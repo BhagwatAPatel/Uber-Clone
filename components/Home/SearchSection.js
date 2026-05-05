251c3732b1149a2bad59095c9d18a3311c1890d0
Bhagwat A Patel <bhagwatp074@gmail.com>
2026-05-05 15:10:49 +0800

    Added a search button for Search Section

--- components/Home/SearchSection.js
+++ components/Home/SearchSection.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function SearchSection() {
   return (
-    <div className="p-2 md:pd-6 border-2 rounded-xl">
+    <div className="p-2 md:p-6 border-2 rounded-xl">
         <p className="text-[20px] font-bold">Get a Ride</p>
         <InputItem type="source" />
         <InputItem type="destination" />
